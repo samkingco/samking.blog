d4e4954679351f3b62f1fe08519faca0ad3291d2
Sam King <mail@samking.co>
2021-09-20 12:51:54 +0100

    Initial next setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-export default MyApp
