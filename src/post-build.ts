5e771d8595138064503aa422bab0c4bc1e085e0b
Sam King <mail@samking.co>
2022-11-04 17:35:30 +0000

    Fix absolute urls

--- src/post-build.ts
+++ src/post-build.ts
@@ -1,23 +1,38 @@
 import { replaceInFile } from "replace-in-file";
 
-// Replace "/${slug}/index.html" with "/${slug}/" for pretty urls
+const baseURL = "https://samking.blog";
+
 (async () => {
-  const timer = "Fixed links in dist .html files";
+  const timer = "Fixed links in dist files";
   console.time(timer);
+  // Replace image links in RSS feed for absolute urls
   await replaceInFile({
     files: "dist/**/rss.xml",
     from: /\/icon-round-144.png/g,
-    to: "https://samking.blog/icon-round-144.png",
+    to: `${baseURL}/icon-round-144.png`,
   });
+  await replaceInFile({
+    files: "dist/**/rss.xml",
+    from: /src="\//g,
+    to: `src="${baseURL}/`,
+  });
+  // Replace relative links for absolute links in RSS feed
+  await replaceInFile({
+    files: "dist/**/rss.xml",
+    from: /href=".\//g,
+    to: `href="${baseURL}/`,
+  });
+  // Replace "/${slug}/index.html" with "/${slug}/" for pretty urls
   await replaceInFile({
     files: "dist/**/*.html",
     from: /\/index.html/g,
     to: "/",
   });
+  // Replace social image links for absolute urls
   await replaceInFile({
     files: "dist/**/*.html",
     from: /content="\/social\//g,
-    to: 'content="https://samking.blog/social/',
+    to: `content="${baseURL}/social/`,
   });
   console.timeEnd(timer);
 })().catch((err) => {
